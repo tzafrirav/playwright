dc590a5a9952dc811a964954b737e5a24bde78ce
צפריר אברהם <tzafrirav@pazgas.co.il>
2025-06-30 13:13:39 +0300

    commit

--- playwright.config.js
+++ playwright.config.js
@@ -23,14 +23,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CB_AGENT ? '@cloudbeat/playwright' : 'html',
+ 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
+    video: "on-first-retry",
+    // screenshot: "on-first-retry",
+    headless: process.env.CB_AGENT ? true : false
   },
 
   /* Configure projects for major browsers */
